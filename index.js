3c94db9fc86b9f5f5bbc0d7c6045ac9c66985b5c
MarcCloud <marccloud.11@gmail.com>
2015-12-20 23:41:04 -0600

    Add main function.

---
+++ index.js
@@ -0,0 +1,12 @@
+import { isNil, map, filter, keys, head, compose, curry } from 'ramda';
+import { Maybe } from 'ramda-fantasy';
+import match from './lib/url-match';
+
+// notNil:: Any -> Boolean
+const notNil = (x) => !isNil(x);
+// executeHandler:: (Object) -> Object -> Any
+const executeHandler = curry((handler, ctx)=> handler(ctx));
+// routeState:: Object -> String -> String -> Any
+const routeState = curry((routingTable, url, route)=> Maybe.maybe(null, executeHandler(routingTable[route]), match(route, url)));
+// routerBuilder:: Object -> (String -> Any)
+export default curry((routingTable, url)=> compose(head, filter(notNil), map(routeState(routingTable,url)), keys)(routingTable));
--- test/index.js
+++ test/index.js
@@ -1,1 +1,43 @@
 import './url-match_test';
+import test from 'blue-tape';
+import { spy } from 'sinon';
+import routes from '../';
+
+
+test('routes should be a function', expect=>{
+    expect.equal(typeof routes, 'function');
+    expect.end();
+});
+
+test('routes should return another function', expect=>{
+    expect.equal(typeof routes(), 'function');
+    expect.end();
+});
+
+test('monarch("/route") should return the same result as routeHandler()', expect=>{
+    const routeHandler = ()=> 'handled!!';
+    const routingTable = { '/route': routeHandler};
+    const monarch = routes(routingTable);
+    expect.equal(monarch('/route'), routeHandler());
+    expect.end();
+});
+
+test('monarch("/users/mark") should call usersHandler with { url: "/users/mark", params:{name:"mark"}}', expect=>{
+    const context = {url: '/users/mark', params:{name: 'mark'}, query: {}};
+    const usersHandler = ({url, params})=> `handled user ${params.name} from ${url}`;
+    const double07 = spy(usersHandler);
+    const routingTable = {'/users/:name': double07};
+    const monarch = routes(routingTable);
+    let result = monarch('/users/mark');
+    expect.ok(double07.calledWith(context));
+    expect.equal(result, usersHandler(context));
+    expect.end();
+});
+
+test('monarch should support multiple routes', expect=>{
+    const routingTable = { '/users': ()=> 'handled first', '/users/:name': ({params})=> `handled ${params.name}`};
+    const monarch = routes(routingTable);
+    let result = monarch('/users/mark');
+    expect.equal(result, 'handled mark');
+    expect.end();
+});
